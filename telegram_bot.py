2f678635a2d3758e050e3ed7d3df0d7343c164d0
Markus Städler <markus.staedler@hotmail.de>
2022-09-29 17:28:20 +0200

    Feat: Added interactive greeting feature to bot in 1:1 chats

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,7 @@
 import dotenv
 import os
 import telegram
+import telegram.ext
 
 ENV_VALUES = dotenv.dotenv_values(".env")
 TELEGRAM_BOT_API_TOKEN = ENV_VALUES["TELEGRAM_BOT_API_TOKEN"]
@@ -22,6 +23,23 @@ def post_message(message: str):
     return True
 
 
+def start(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        rf"Hi {user.mention_markdown_v2()}\!",
+        reply_markup=telegram.ForceReply(selective=True)
+    )
+
+
 if __name__ == "__main__":
-    m = "Hello Channel"
-    post_message(m)
+    #m = "Hello Channel"
+    #post_message(m)
+    updater = telegram.ext.Updater(TELEGRAM_BOT_API_TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(telegram.ext.CommandHandler("start", start))
+
+    updater.start_polling()
+
+    updater.idle()
