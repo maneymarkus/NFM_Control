7410e5498fe4f66b2cc82a7ad1178a1b3d84cde1
Markus Städler <markus.staedler@hotmail.de>
2022-11-09 17:30:01 +0100

    Feat: Added `__init__.py` to app directory

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+import app.utils as utils
+from app.frequency_controller import FrequencyController
+from app.frequency_sensor import FrequencySensor
+from app.frequency_sensor_mock import FrequencySensorMock
