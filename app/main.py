e90e0d59f5854056ab7c5ddf3eb62f39931fe733
Markus Städler <markus.staedler@hotmail.de>
2022-11-09 17:30:59 +0100

    Refactor: The `main.py` is now solely the main entry point into the whole application

--- app/main.py
+++ app/main.py
@@ -1,35 +1,19 @@
-import re
-import requests
+from absl import logging
 import time
+
+from frequency_sensor_mock import FrequencySensorMock
+from frequency_controller import FrequencyController
 
 
 def main():
-    url = "https://netzfrequenzmessung.de:9080/frequenz01.xml"
-    headers = {
-        "content-type": "text/plain"
-    }
+    logging.set_verbosity(logging.DEBUG)
 
-    frequency_time_series = []
-
-    while True:
-        time.sleep(1 - time.monotonic() % 1)
-        response = get_data(url, headers)
-        response_body = response.text
-
-        frequency = float(extract_tag_content(response_body, "f"))
-        print(frequency)
-        frequency_time_series.append(frequency)
-
-
-def get_data(url: str, headers: dict) -> requests.models.Response:
-    session = requests.Session()
-    response = session.get(url, headers=headers, verify=False)
-    return response
-
-
-def extract_tag_content(text: str, tag_name: str) -> str:
-    tag_content = re.search(f"<{tag_name}>(.+)</{tag_name}>", text).group(1)
-    return tag_content
+    f_sensor = FrequencySensorMock()
+    f_controller = FrequencyController(f_sensor)
+    f_controller.start()
+    time.sleep(5)
+    f_controller.stop()
+    print(f_controller.queue.qsize())
 
 
 if __name__ == '__main__':
